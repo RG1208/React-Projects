27a207a98fe0991dcb0a4dcc678837696c10c99b
RG1208 <rachitgarg1208@gmail.com>
2025-01-15 02:09:01 +0530

    feat: Login Page

--- 01ems/src/App.jsx
+++ 01ems/src/App.jsx
@@ -1,14 +1,13 @@
 import { useState } from 'react'
 import './App.css'
+import Login from './components/Auth/Login'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <h1 className='bg-red-50'>HI</h1>
-      </div>
+     <Login />
     </>
   )
 }
---
+++ 01ems/src/components/Auth/Login.jsx
@@ -0,0 +1,58 @@
+import React, { useState } from 'react'
+
+function Login() {
+
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+
+  const submitHandler = (e) => { 
+    e.preventDefault()
+    console.log("Form Submitted"); 
+    console.log("email is :",email);
+    console.log("password is:",password);
+    
+    setEmail("")
+    setPassword("")
+   }
+
+  return (
+    <>
+      <div className="flex items-center justify-center h-screen w-screen">
+        <div>
+          <form 
+          onSubmit={submitHandler}
+          className="flex flex-col justify-center items-center">
+
+            <input
+              required
+              value={email}
+              onChange={(e) => { 
+                setEmail(e.target.value)
+               }}
+              className="m-2 px-4 py-2 w-80 border-2 border-red-800 rounded-2xl bg-transparent placeholder:text-gray-500 text-gray-400 focus:outline-none"
+              type="email"
+              placeholder="Email"
+            />
+
+            <input
+              required
+              value={password}
+              onChange={(e) => { 
+                setPassword(e.target.value)
+               }}
+              className="m-2 px-4 py-2 w-80 border-2 border-red-800 rounded-2xl bg-transparent placeholder:text-gray-500 text-gray-400 focus:outline-none"
+              type="password"
+              placeholder="Password"
+            />
+
+            <button className="mt-3 h-10 w-80 rounded-full bg-red-900 text-white hover:bg-red-700 transition duration-200">
+              Login
+            </button>
+          </form>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Login;
