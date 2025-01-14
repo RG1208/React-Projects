ea1ebebc9a8635bb4067077408f77af0b27b345e
RG1208 <rachitgarg1208@gmail.com>
2025-01-14 17:24:32 +0530

    React-Projects

---
+++ 01ems/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <h1 className='bg-red-50'>HI</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
