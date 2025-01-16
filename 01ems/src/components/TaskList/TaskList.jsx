5c91752b54f2723dcc888cf2ae1fb55cd729178b
RG1208 <rachitgarg1208@gmail.com>
2025-01-16 13:59:37 +0530

    feat: Employee Dashboard UI Completed

--- 01ems/src/components/TaskList/TaskList.jsx
+++ 01ems/src/components/TaskList/TaskList.jsx
@@ -3,20 +3,66 @@ import React from 'react'
 function TaskList() {
   return (
     <div id='taskList' className='text-white flex overflow-x-auto items-center justify-start gap-5 flex-nowrap py-5 h-[55%] w-full mt-10'>
-        <div className='bg-yellow-400 flex-shrink-0 h-full w-[300px] rounded-xl'>
+        <div className='bg-yellow-400 flex-shrink-0 p-3 h-full w-[390px] rounded-xl'>
+          <div className='flex justify-between items-center'>
+            <h3 className='bg-red-600 px-3 py-1 rounded text-sm font-semibold'>High</h3>
+            <h4 className='text-sm font-semibold'>20 Feb 2024</h4>
+          </div>
+          <h2 className='mt-5 text-2xl font-semibold'>Make a Youtube Video</h2>
+          <p className='text-sm mt-2'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime deleniti possimus, esse mollitia necessitatibus aliquam.</p>
         </div>
-        <div className='bg-red-400 flex-shrink-0 h-full w-[300px] rounded-xl'>
+
+        
+        <div className='bg-red-400 flex-shrink-0 p-3 h-full w-[390px] rounded-xl'>
+          <div className='flex justify-between items-center'>
+            <h3 className='bg-red-600 px-3 py-1 rounded text-sm font-semibold'>High</h3>
+            <h4 className='text-sm font-semibold'>20 Feb 2024</h4>
+          </div>
+          <h2 className='mt-5 text-2xl font-semibold'>Make a Youtube Video</h2>
+          <p className='text-sm mt-2'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime deleniti possimus, esse mollitia necessitatibus aliquam.</p>
         </div>
-        <div className='bg-orange-400 flex-shrink-0 h-full w-[300px] rounded-xl'>
+
+        
+        <div className='bg-orange-400 flex-shrink-0 p-3 h-full w-[390px] rounded-xl'>
+          <div className='flex justify-between items-center'>
+            <h3 className='bg-red-600 px-3 py-1 rounded text-sm font-semibold'>High</h3>
+            <h4 className='text-sm font-semibold'>20 Feb 2024</h4>
+          </div>
+          <h2 className='mt-5 text-2xl font-semibold'>Make a Youtube Video</h2>
+          <p className='text-sm mt-2'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime deleniti possimus, esse mollitia necessitatibus aliquam.</p>
         </div>
-        <div className='bg-blue-400 flex-shrink-0 h-full w-[300px] rounded-xl'>
+
+        
+        <div className='bg-purple-400 flex-shrink-0 p-3 h-full w-[390px] rounded-xl'>
+          <div className='flex justify-between items-center'>
+            <h3 className='bg-red-600 px-3 py-1 rounded text-sm font-semibold'>High</h3>
+            <h4 className='text-sm font-semibold'>20 Feb 2024</h4>
+          </div>
+          <h2 className='mt-5 text-2xl font-semibold'>Make a Youtube Video</h2>
+          <p className='text-sm mt-2'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime deleniti possimus, esse mollitia necessitatibus aliquam.</p>
         </div>
-        <div className='bg-green-400 flex-shrink-0 h-full w-[300px] rounded-xl'>
+
+        
+        <div className='bg-blue-400 flex-shrink-0 p-3 h-full w-[390px] rounded-xl'>
+          <div className='flex justify-between items-center'>
+            <h3 className='bg-red-600 px-3 py-1 rounded text-sm font-semibold'>High</h3>
+            <h4 className='text-sm font-semibold'>20 Feb 2024</h4>
+          </div>
+          <h2 className='mt-5 text-2xl font-semibold'>Make a Youtube Video</h2>
+          <p className='text-sm mt-2'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime deleniti possimus, esse mollitia necessitatibus aliquam.</p>
         </div>
-        <div className='bg-purple-400  flex-shrink-0 h-full w-[300px] rounded-xl'>
+
+        
+        <div className='bg-green-400 flex-shrink-0 p-3 h-full w-[390px] rounded-xl'>
+          <div className='flex justify-between items-center'>
+            <h3 className='bg-red-600 px-3 py-1 rounded text-sm font-semibold'>High</h3>
+            <h4 className='text-sm font-semibold'>20 Feb 2024</h4>
+          </div>
+          <h2 className='mt-5 text-2xl font-semibold'>Make a Youtube Video</h2>
+          <p className='text-sm mt-2'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime deleniti possimus, esse mollitia necessitatibus aliquam.</p>
         </div>
-        <div className='bg-teal-400 flex-shrink-0 h-full w-[300px] rounded-xl'>
-        </div>
+
+        
     </div>
   )
 }
